3fb1fb9b193a240b41b6d461f3bbf54f4fd33c5d
Dev Madlani <madlanidev@gmail.com>
2025-11-02 22:01:21 +0530

    add husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig(
         },
         rules: {
             // 'no-console': 'error',
+            // 'dot-notation': 'error',
         },
     },
 )
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 function welcome(name: string) {
     console.log('Hello ' + name)
+    const user = {
+        name: 'dev',
+    }
+    return user.name
 }
 
 welcome('dev')
